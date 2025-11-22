ef0bc099f3aa834caacfe852c38a0f6a2dd6e5ed
zhangjiaxing666 <zhang_jia_xing666@163.com>
2025-11-22 23:21:24 +0800

    Delete Downloads/basic-android-kotlin-compose-training-affirmations-starter/basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/model directory
    delete

--- Downloads/basic-android-kotlin-compose-training-affirmations-starter/basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++
@@ -1,8 +0,0 @@
-package com.example.affirmations.modle
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmation(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
-)
